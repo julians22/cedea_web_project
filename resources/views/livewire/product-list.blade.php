1008bd3aec6ffdeb8263ef95d566e921fff86aa2
Ghassan Fs <fsghassan2429d@gmail.com>
2024-08-21 13:03:01 +0700

    undo arrow-right component

--- resources/views/livewire/product-list.blade.php
+++ resources/views/livewire/product-list.blade.php
@@ -149,7 +149,19 @@
                                             {{-- <x-arrow-right class="inline-block lg:hidden" /> --}}
                                         </div>
 
-                                        <x-arrow-right class="max-lg:hidden" />
+                                        <div class="cursor-pointer text-cedea-red max-md:hidden">
+                                            <svg class="h-5" xmlns="http://www.w3.org/2000/svg"
+                                                xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 17.78 32.83">
+                                                <g>
+                                                    <g style="fill: none; filter: url(#d);">
+                                                        <polyline class="fill-none stroke-cedea-red"
+                                                            points="1.36 .75 16.72 16.11 .75 32.07"
+                                                            style="fill: none; stroke-linecap: round; stroke-miterlimit: 10; stroke-width: 2px;" />
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        </div>
+
                                     </div>
                                 </div>
                             </div>
