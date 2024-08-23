07c30d5a4ec59d001765f8549db3e13baaa13f79
Ghassan Fs <fsghassan2429d@gmail.com>
2024-08-23 13:49:44 +0700

    update product list error icon responsive size

--- resources/views/livewire/product-list.blade.php
+++ resources/views/livewire/product-list.blade.php
@@ -169,7 +169,7 @@
                     @empty
                         <div
                             class="col-span-full flex flex-col items-center justify-center self-center justify-self-center">
-                            <lord-icon class="size-40 inline-block" src="https://cdn.lordicon.com/rmkpgtpt.json"
+                            <lord-icon class="~size-5/40 inline-block" src="https://cdn.lordicon.com/rmkpgtpt.json"
                                 trigger="in" delay="500" state="in-reveal"
                                 colors="primary:#e4e4e4,secondary:#e4e4e4">
                             </lord-icon>
