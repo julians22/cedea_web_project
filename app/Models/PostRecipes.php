543b6c6ab49ac4635b9c3a77aa19fe3b9f3b56e4
Ghassan Fs <fsghassan2429d@gmail.com>
2024-09-10 13:39:34 +0700

    initial sitemap setup

--- app/Models/PostRecipes.php
+++ app/Models/PostRecipes.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Sitemap\Tags\Url;
 use Spatie\Translatable\HasTranslations;
 
 class PostRecipes extends Model implements HasMedia
@@ -36,5 +38,18 @@ class PostRecipes extends Model implements HasMedia
             ->usingLanguage('id')
             ->generateSlugsFrom('title')
             ->saveSlugsTo('slug');
+    }
+
+    /**
+     * Get the sitemap tag for the resource.
+     *
+     * @return \Spatie\Sitemap\Tags\Url|string|array
+     */
+    public function toSitemapTag(): Url | string | array
+    {
+        return Url::create(route('recipe.detail', $this))
+            ->setLastModificationDate(Carbon::create($this->updated_at))
+            ->setChangeFrequency(Url::CHANGE_FREQUENCY_YEARLY)
+            ->setPriority(0.1);
     }
 }
