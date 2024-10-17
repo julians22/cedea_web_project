a0b73c51604418d8b6a25e6cab131e16e0c58f26
Ghassan Fs <fsghassan2429d@gmail.com>
2024-10-17 10:25:20 +0700

    hotfix search modal mobile

--- resources/views/components/search-modal.blade.php
+++ resources/views/components/search-modal.blade.php
@@ -12,20 +12,19 @@
     </button>
 
     <form
-        class="min-w-28 min-h-8 has-[:focus]:border-cedea-red-500/50 has-[:active]:border-cedea-red-500/5has-[:focus]:border-cedea-red-500/50 absolute right-4 top-full z-10 flex translate-y-[150%] items-center justify-between gap-x-4 rounded-full border bg-white px-4 py-2 outline-cedea-red-500 navbar-small:translate-y-1/2 md:right-0 lg:translate-y-full"
+        class="md:min-w-28 min-h-8 has-[:focus]:border-cedea-red-500/50 has-[:active]:border-cedea-red-500/5has-[:focus]:border-cedea-red-500/50 absolute right-4 top-full z-10 flex max-w-[90dvw] translate-y-[150%] items-center justify-between gap-x-4 rounded-full border bg-white px-4 py-2 outline-cedea-red-500 navbar-small:translate-y-1/2 md:right-0 lg:translate-y-full"
         x-cloak x-trap.inert.noscroll="seachModalOpen" x-show="seachModalOpen"
         x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0"
         x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in duration-300"
         x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" action="{{ route('search') }}"
         method="GET" @keyup.enter="$event.target.submit(); seachModalOpen=false">
 
-        <input class="p-0.5 text-cedea-red-500 outline-none placeholder:text-cedea-red-500/50" id="query"
+        <input class="w-full p-0.5 text-cedea-red-500 outline-none placeholder:text-cedea-red-500/50" id="query"
             name="query" type="text" placeholder="Cari di sini">
 
         <button type="submit">
             <x-icon.magnifying-glass class="text-cedea-red-500" />
         </button>
-
     </form>
 
 </div>
