81da3df2b76215e585496418574b979e34683864
Ghassan Fs <fsghassan2429d@gmail.com>
2025-01-07 14:06:31 +0700

    try to fix media path error

--- app/Filament/Resources/NewsResource.php
+++ app/Filament/Resources/NewsResource.php
@@ -131,6 +131,7 @@ class NewsResource extends Resource
                 )->from('md'),
                 TiptapEditor::make('content')
                     ->profile('default')
+                    ->directory('public')
                     ->acceptedFileTypes(['image/*'])
                     ->translatable(true, null, [
                         'id' => ['required',],
