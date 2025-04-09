87951646643022cd1c779967f7fade4760ee2f7b
Ghassan Fs <fsghassan2429d@gmail.com>
2025-04-09 11:37:18 +0700

    move new product order to start

--- app/Filament/Resources/ProductResource/Pages/CreateProduct.php
+++ app/Filament/Resources/ProductResource/Pages/CreateProduct.php
@@ -9,4 +9,10 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateProduct extends CreateRecord
 {
     protected static string $resource = ProductResource::class;
+
+    protected function afterCreate(): void
+    {
+        $product = $this->record;
+        $product->moveToStart();
+    }
 }
