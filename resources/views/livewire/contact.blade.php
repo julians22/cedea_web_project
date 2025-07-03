02996d1ddb196ecdb9ebf9fe4cd23afe18a3eaba
Ghassan Fs <fsghassan2429d@gmail.com>
2025-07-03 15:22:04 +0700

    update ux

--- resources/views/livewire/contact.blade.php
+++ resources/views/livewire/contact.blade.php
@@ -158,8 +158,8 @@
                                             d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
                                         </path>
                                     </svg>
-                                    <span x-show="!loading">{{ __('contact.submit') }}</span>
-                                    <span x-show="loading">Processing...</span>
+                                    <span x-show="!loading" wire:loading.remove>{{ __('contact.submit') }}</span>
+                                    <span x-show="loading" wire:loading>Processing...</span>
                                 </span>
                             </button>
 
