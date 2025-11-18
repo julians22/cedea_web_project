90bc24f6e17f2bb42f2ef3e8a4ee31ba2eef1f63
Ghassan Fs <fsghassan2429d@gmail.com>
2025-11-18 11:39:22 +0700

    add rte redirect

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,21 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 |
 */
 
+if (env('RTE_CAMPAIGN', false)) {
+    Route::redirect('eomuk-bar-chill-in-seoul', 'https://eomuk-bar-rte.cedeaseafood.com')->name('redirect-eomuk-bar-chill-in-seoul');
+}
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
 Route::group([
     'prefix' => LaravelLocalization::setLocale(),
     'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath'],
