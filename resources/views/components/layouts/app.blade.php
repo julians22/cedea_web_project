0d6200971cd7bdf1e10b1447a47b5dd27f631aad
Bumpyy <fsghassan2429d@gmail.com>
2026-05-25 13:56:20 +0700

    update update gtag implementation

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -29,35 +29,10 @@
                 dataLayer.push(arguments);
             }
             gtag('js', new Date());
+
             gtag('config', 'G-57ZD2ZE0ME');
-            gtag('set', 'allow_google_signals', true);
-        </script>
-        <!-- Google Tag Manager -->
-
-        <script>
-            (function(w, d, s, l, i) {
-                w[l] = w[l] || [];
-                w[l].push({
-                    'gtm.start':
-
-                        new Date().getTime(),
-                    event: 'gtm.js'
-                });
-                var f = d.getElementsByTagName(s)[0],
-
-                    j = d.createElement(s),
-                    dl = l != 'dataLayer' ? '&l=' + l : '';
-                j.async = true;
-                j.src =
-
-                    'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-                f.parentNode.insertBefore(j, f);
-
-            })
-            (window, document, 'script', 'dataLayer', 'GTM-TJJ7HQWD');
         </script>
 
-        <!-- End Google Tag Manager -->
     @endproduction
 
     @stack('plugin-scripts')
@@ -65,14 +40,14 @@
 
 <body class="font-poppins antialiased">
 
-    @production
+    {{-- @production
         <!-- Google Tag Manager (noscript) -->
 
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TJJ7HQWD" height="0" width="0"
                 style="display:none;visibility:hidden"></iframe></noscript>
 
         <!-- End Google Tag Manager (noscript) -->
-    @endproduction
+    @endproduction --}}
 
 
     <x-header />
